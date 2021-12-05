e8619d76e5363c64adf854fee1cd4d1c233c20cd
FokinBogdan <fokin.boghdan@mail.ru>
2021-12-05 15:27:26 +0500

    Этап №2: Создание JSON с текстовым содержимым всех тегов внутри item

--- 2 Task.py
+++ 2 Task.py
@@ -2,27 +2,16 @@ import xml.etree.ElementTree as ET
 from urllib.request import urlopen
 import json
 
+data = urlopen('https://lenta.ru/rss').read().decode('utf8')
+root = ET.fromstring(data)
+data_dict = []
 
-def get_first_element_tree():
-    data = urlopen('https://lenta.ru/rss').read().decode('utf8')
-    root = ET.fromstring(data)
-    return root[0]
+for i in root[0].findall('item'):
+    a = {}
+    for j in i:
+        a[j.tag] = j.text
+    data_dict.append(a)
 
-
-def get_items(first_element):
-    items = []
-    for i in first_element.findall('item'):
-        items.append({'author': i.find('author').text, 'category': i.find('category').text,
-                      'description': i.find('description').text, 'enclosure': i.find('enclosure').text,
-                      'guid': i.find('guid').text, 'link': i.find('link').text,
-                      'pubDate': i.find('pubDate').text, 'title': i.find('title').text})
-    return items
-
-
-def save_json(first_element):
-    with open("Content of item tags.txt", 'wb') as file:
-        json_file = json.dumps(get_items(first_element), ensure_ascii=False, indent=3).encode('utf8')
-        file.write(json_file)
-
-first_element_tree = get_first_element_tree()
-save_json(first_element_tree)
+with open('news.json', 'wb') as json_file:
+    file = json.dumps(data_dict, indent=3, ensure_ascii=False).encode('utf8')
+    json_file.write(file)

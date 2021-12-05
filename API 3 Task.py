61c85550eee9b889fc0bfb70ad48eb3468ea034c
FokinBogdan <fokin.boghdan@mail.ru>
2021-12-05 15:39:29 +0500

    API, ДЗ

--- API 3 Task.py
+++ API 3 Task.py
@@ -10,7 +10,7 @@ def get_datetime(comment_info):
 url = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%93%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%B8%D0%B9,_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80_%D0%91%D0%BE%D1%80%D0%B8%D1%81%D0%BE%D0%B2%D0%B8%D1%87'
 
 data = loads(urlopen(url).read().decode('utf8'))
-commit_info = data['query']['pages']['183903']['revisions']
+comment_info = data['query']['pages']['183903']['revisions']
 
-for key, group_items in groupby(commit_info, key=get_datetime):
+for key, group_items in groupby(comment_info, key=get_datetime):
     print(key, sum(1 for _ in group_items))
